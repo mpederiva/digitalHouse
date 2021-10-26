29c99cd69756dcf2f5b7550eddf62528a5148fe1
Marta Pederiva <80361071+mpederiva@users.noreply.github.com>
2021-10-25 21:05:40 -0300

    Add files via upload

---
+++ programacaoImperativa/1025/ex2.js
@@ -0,0 +1,7 @@
+let jogadora = "Emma";
+let numero = 91;
+let jogadoraAtiva = true;
+ 
+console.log("Nome da Jogadora:", jogadora);
+console.log("Número da Jogadora:", numero);
+console.log("Jogadora ativa?",jogadoraAtiva);
---
+++ programacaoImperativa/1025/ex3.js
@@ -0,0 +1,27 @@
+//1) Em um novo arquivo chamado ex3.js, exiba na tela a soma de dois números.
+console.log(91+93);
+
+//2) Repita o processo anterior, mas utilizando outro operador.
+console.log(91*93);
+
+//3) Declare e atribua 2 variáveis ​​numéricas independentes. Exiba a soma de ambas na tela.
+let numero1 = 91;
+let numero2 = 93;
+
+var soma = (numero1+numero2);
+console.log(soma);
+
+//4) Atribua a soma das duas variáveis ​​anteriores a uma outra variável e exiba-a na tela.
+let numero3 = 90;
+var somaNova = (soma+numero3);
+console.log(somaNova);
+
+//5) Re-atribua à variável anterior "1" +1, e exiba na tela. (O que  aconteceu?)
+var numero1Mais1 = (numero1+1);
+console.log(numero1Mais1);
+ 
+//6) Re-atribua "olá" + 23, e exiba na tela. (O que aconteceu?)
+console.log("Olá" + 23);
+
+//7) Crie mensagens concatenadas com valores diferentes.
+console.log("Se " + numero1 + " + " + numero2 + " = " + soma + ", quando eu adiciono " + numero3 + ", o resultado final é " + somaNova); 
