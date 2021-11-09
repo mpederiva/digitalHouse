aff546fcb37da7c9f61d46dd2c0fc7247f7e6f51
Marta Pederiva <80361071+mpederiva@users.noreply.github.com>
2021-11-09 20:47:12 -0300

    Add files via upload

---
+++ programacaoImperativa/1026/mesa.js
@@ -0,0 +1,44 @@
+//Micro Desafio 1
+function teste1(x, y) {
+    return y - x
+}
+console.log(teste1(10,40));
+
+//Micro Desafio 1
+function teste2(x, y) {
+    return x * 2
+    console.log(x)
+    return x/2
+}
+console.log(teste2(10));
+
+//1. Crie uma função que converta polegadas em centímetros. 
+//Recebe pelo parâmetro polegadas e retorna seu equivalente em centímetros.
+function polegada(x, y) {
+    return x * 2.54
+    console.log(x)
+    return x/2
+}
+console.log(polegada(10) + " cm");
+
+//2. Crie uma função que receba uma string e a converta em um URL. 
+//ex: "funcaojs" é retornado como "http://www.funcaojs.com.br"
+
+//3. Crie uma função que recebe uma string e retorna a mesma frase, mas com o caracter de exclamação ( ! ).
+
+//4. Crie uma função que calcule a idade dos cachorros, considerando que 1 ano humano, equivale a 7 anos para eles.
+
+//5. Crie uma função que calcule o valor da sua hora de trabalho, tendo como parâmetro o seu salário mensal. 
+//PS: considere que você trabalhe 160 horas no mês.
+
+//6. Crie a função calculadora de IMC de uma pessoa, que recebe a altura em centímetros e o peso em quilogramas.
+//Em seguida, execute a função, testando diferentes valores.
+
+//7. Crie uma função que receba uma string em minúsculas, converta-a em maiúsculas e as retorne. 
+//Investigue o que o método de .toUpperCase() faz.
+
+//8. Crie uma função que receba um parâmetro e retorne o tipo de dado deste parâmetro. 
+//Dica: Isso te ajudará a entender o que o operador typeof faz.
+
+//9. Crie uma função pela qual passamos o raio de um círculo e ela retorna a circunferência. 
+//Dica: Investigue se o objeto Math tem entre suas propriedades o número Pi.
