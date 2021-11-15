c305cf67c903fb5ff01a4501e1b3b875f4b7effb
Marta Pederiva <80361071+mpederiva@users.noreply.github.com>
2021-11-15 03:10:30 -0300

    Upload script.js

---
+++ script.js
@@ -0,0 +1,41 @@
+function displayDatabase(){
+    var arrow = document.getElementById('animation');
+    arrow.classList.toggle('rotate');
+    var display = document.getElementById('Database');
+    display.classList.toggle('active');
+}
+
+function displayIntroComputerScience(){
+    var arrow = document.getElementById('animatio');
+    arrow.classList.toggle('rotate');
+    var display = document.getElementById('introComputerScience');
+    display.classList.toggle('active');
+}
+function displayLearningAgility(){
+    var arrow = document.getElementById('animati');
+    arrow.classList.toggle('rotate');
+    var display = document.getElementById('learningAgility');
+    display.classList.toggle('active');
+    console.log (display);
+}
+
+function displayFrontEndI(){
+    var arrow = document.getElementById('animat');
+    arrow.classList.toggle('rotate');
+    var display = document.getElementById('frontEndI');
+    display.classList.toggle('active');
+}
+
+function displayAgileMethodologies(){
+    var arrow = document.getElementById('anima');
+    arrow.classList.toggle('rotate');
+    var display = document.getElementById('agileMethodologies');
+    display.classList.toggle('active');
+}
+
+function displayImperativeProgramming(){
+    var arrow = document.getElementById('ani');
+    arrow.classList.toggle('rotate');
+    var display = document.getElementById('imperativeProgramming');
+    display.classList.toggle('active');
+}
